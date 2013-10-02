0c69084751322ad365c676e3580cb3d932d3938b
Carlos Escribano Rey <carlos@markhaus.com>
2013-10-02 11:56:22 +0200

    v1.1.2
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- catalog/doofinder.php
+++ catalog/doofinder.php
@@ -58,6 +58,9 @@ define('DOOFINDER_SHOW_FINAL_PRICES', true);
 
 define('DOOFINDER_CHUNK_SIZE', 1000);
 
+if (! defined('ENT_HTML401'))
+  define('ENT_HTML401', 0);
+
 // =============================================================== DO NOT EDIT!!
 
 if (! function_exists('tep_get_version'))
@@ -85,7 +88,7 @@ if (! function_exists('tep_get_version'))
 
 class DoofinderFeed
 {
-  const VERSION = "1.1.1";
+  const VERSION = "1.1.2";
 
   protected $_aLimit;
   protected $_iChunkSize;
@@ -133,6 +136,8 @@ class DoofinderFeed
 
     $this->_sProductURL = FILENAME_PRODUCT_INFO;
     $this->_sProductURLParams = "currency=" . $this->_sCurrencyCode . "&products_id=";
+
+    $this->_bMbOk = is_callable('mb_detect_encoding');
   }
 
   /**
@@ -213,34 +218,34 @@ class DoofinderFeed
         echo $product->id.$FS;
 
         // title
-        $productTitle = self::clean($product->title);
+        $productTitle = $this->clean($product->title);
         echo $productTitle.$FS;
 
         // link
         echo $this->getProductURL($product->id).$FS;
 
         // description
-        echo self::clean($product->description).$FS;
+        echo $this->clean($product->description).$FS;
 
         // image_link
         echo $this->getProductImageUrl($product->image_url).$FS;
 
         // categories
-        echo implode($this->_sSepCategory, $this->_productCategories($product->id)).$FS;
+        echo $this->clean(implode($this->_sSepCategory, $this->_productCategories($product->id))).$FS;
 
         // availability
         echo ($product->quantity > 0 ? "in stock" : "out of stock").$FS;
 
         // brand
-        echo self::clean($product->brand).$FS;
+        echo $this->clean($product->brand).$FS;
 
         // mpn
-        echo self::clean($product->mpn).$FS;
+        echo $this->clean($product->mpn).$FS;
 
         if ($this->_bShowPrices)
         {
           // price
-          $price = $this->getPrice($product->price, self::clean($product->tax_rate));
+          $price = $this->getPrice($product->price, $this->clean($product->tax_rate));
 
           if ($price > 0)
             echo $price.$FS;
@@ -248,7 +253,7 @@ class DoofinderFeed
             echo $FS;
 
           // sale_price
-          $sale_price = $this->getPrice($product->sale_price, self::clean($product->tax_rate));
+          $sale_price = $this->getPrice($product->sale_price, $this->clean($product->tax_rate));
 
           if ($sale_price > 0)
             echo $sale_price.$FS;
@@ -257,10 +262,10 @@ class DoofinderFeed
         }
 
         // extra_title_1
-        echo self::cleanReferences($productTitle).$FS;
+        echo $this->cleanReferences($productTitle).$FS;
 
         // extra_title_2
-        echo self::splitReferences($productTitle);
+        echo $this->splitReferences($productTitle);
 
         echo PHP_EOL;
         flush();ob_flush();
@@ -640,12 +645,17 @@ class DoofinderFeed
 
   // Cleaning
 
-  public static function clean($text)
+  public function clean($text)
   {
     // http://stackoverflow.com/questions/4224141/php-removing-invalid-utf-8-characters-in-xml-using-filter
     $valid_utf8 = '/([\x09\x0A\x0D\x20-\x7E]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]|\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F][\x80-\xBF]{2})|./x';
 
-    $text = strip_tags(html_entity_decode($text, ENT_QUOTES, 'UTF-8'));
+    if ($this->_bMbOk)
+    {
+      $text = mb_convert_encoding($text, "UTF-8");
+    }
+
+    $text = strip_tags(html_entity_decode($text, ENT_QUOTES|ENT_HTML401, "UTF-8"));
     $text = preg_replace('/[ ]{2,}/', ' ', $text);
     $text = preg_replace('/[\t\s]+|[|\r\n]/', " ", $text);
     $text = str_replace(array(chr(9), chr(10)), " ", $text);
@@ -653,13 +663,13 @@ class DoofinderFeed
     return preg_replace($valid_utf8, '$1', trim($text));
   }
 
-  public static function cleanReferences($text)
+  public function cleanReferences($text)
   {
     $forbidden = array('-');
     return str_replace($forbidden, "", $text);
   }
 
-  public static function splitReferences($text)
+  public function splitReferences($text)
   {
     return preg_replace("/([^\d\s])([\d])/", "$1 $2", $text);
   }
